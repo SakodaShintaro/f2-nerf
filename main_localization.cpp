eb6955cd410fe46e0556dcabf9c4ebdf92b847a5
Shintaro Sakoda <sakoda@keio.jp>
2023-04-29 15:42:49 +0900

    Added main_localization.cpp

---
+++ main_localization.cpp
@@ -0,0 +1,11 @@
+#include "src/ExpRunner.h"
+
+#include <torch/torch.h>
+
+#include <iostream>
+#include <memory>
+
+int main(int argc, char * argv[])
+{
+  std::cout << "Localization" << std::endl;
+}
