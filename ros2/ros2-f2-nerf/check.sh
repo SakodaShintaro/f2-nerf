d114b427ba522408a55e012ba5bf45f2cf6c483b
Shintaro Sakoda <sakoda@keio.jp>
2023-05-04 13:34:00 +0900

    Added option

--- ros2/ros2-f2-nerf/check.sh
+++ ros2/ros2-f2-nerf/check.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+set -eux
+
 colcon build
+
+set +u
 source install/setup.bash
+set -u
+
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/root/f2-nerf/External/libtorch/lib/
 ros2 run ros2-f2-nerf nerf_based_localizer
