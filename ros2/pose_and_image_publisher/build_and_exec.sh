b02b068f4cef2945fed82213424e319e08ad5243
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2023-05-09 18:10:58 +0900

    Added build_and_exec.sh

---
+++ ros2/pose_and_image_publisher/build_and_exec.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eux
+
+colcon build
+
+set +u
+source install/setup.bash
+set -u
+
+ros2 run pose_and_image_publisher pose_and_image_publisher ~/data/converted/20230503_raw_colmap_try1/
