fe6dcc9902a2a907e9867eb842c05be4fd7477ed
Shintaro SAKODA <shintaro.sakoda@tier4.jp>
2023-05-11 07:52:18 +0900

    Added print pose

--- scripts/check_npy.py
+++ scripts/check_npy.py
@@ -15,3 +15,6 @@ if __name__ == "__main__":
     args = parse_args()
     npy = np.load(args.path_to_npy_file)
     print(npy.shape)
+    np.set_printoptions(precision=6, suppress=True)
+    pose = npy[0, 0:12].reshape((3, 4))
+    print(pose)
