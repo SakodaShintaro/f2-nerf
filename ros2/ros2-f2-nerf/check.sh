5274cd5397fcc6ac9e85648651ca0a23d9e73051
Shintaro Sakoda <sakoda@keio.jp>
2023-05-12 13:50:22 +0900

    Added debug command

--- ros2/ros2-f2-nerf/check.sh
+++ ros2/ros2-f2-nerf/check.sh
@@ -10,3 +10,6 @@ set -u
 
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/root/f2-nerf/External/libtorch/lib/
 ros2 run ros2-f2-nerf nerf_based_localizer
+
+# Debug
+# ros2 run --prefix 'gdb -ex run --args' ros2-f2-nerf nerf_based_localizer
