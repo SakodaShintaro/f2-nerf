bdbf88ccacc311fe36643bd23a084b8d42a954b2
Shintaro SAKODA <shintaro.sakoda@tier4.jp>
2023-05-18 18:47:18 +0900

    Updated to exec

--- ros2/script/launch_nerf_based_localizer.sh
+++ ros2/script/launch_nerf_based_localizer.sh
@@ -13,4 +13,6 @@ set -u
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/root/f2-nerf/External/libtorch/lib/
 ros2 run ros2-f2-nerf nerf_based_localizer \
     --ros-args --remap image:=/sensing/camera/c1/image_rect_resized \
-    --ros-args --remap initial_pose_with_covariance:=/localization/pose_twist_fusion_filter/biased_pose_with_covariance
+    --ros-args --remap initial_pose_with_covariance:=/localization/pose_twist_fusion_filter/biased_pose_with_covariance \
+    --ros-args --remap nerf_service:=/localization/pose_estimator/ndt_align_srv \
+    --ros-args --remap trigger_node_srv:=/localization/pose_estimator/trigger_node
