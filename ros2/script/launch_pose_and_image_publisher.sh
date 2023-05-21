0d55c4bc5a21bef51625bfa2bbbc23f4850a850d
Shintaro Sakoda <sakoda@keio.jp>
2023-05-21 14:07:01 +0900

    Added plot command

--- ros2/script/launch_pose_and_image_publisher.sh
+++ ros2/script/launch_pose_and_image_publisher.sh
@@ -15,3 +15,5 @@ rm -rf ./result_images/trial
 ros2 service call /trigger_node_srv std_srvs/srv/SetBool "{data: true}"
 
 ros2 run pose_and_image_publisher pose_and_image_publisher ~/data/converted/20230501_try1/
+
+python3 python/analyze_particles_log.py ./result_images/trial/particles/
