192e4c5c4b6f167c73820e4a541be0eac8d9422e
Shintaro Sakoda <sakoda@keio.jp>
2023-06-01 11:54:57 +0900

    Added launch_rosbag.sh

---
+++ ros2/script/launch_rosbag.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -eux
+
+cd $(dirname $0)/../
+
+colcon build --symlink-install --packages-up-to pose_and_image_publisher --cmake-args -DCMAKE_BUILD_TYPE=Release
+
+set +u
+source install/setup.bash
+set -u
+
+rm -rf ./result_images/trial
+
+ros2 service call /trigger_node_srv std_srvs/srv/SetBool "{data: true}"
+
+ros2 bag play --rate 1 $1 \
+              --topics /sensing/camera/c1/image_rect_resized \
+                       /localization/pose_twist_fusion_filter/biased_pose_with_covariance \
+                       /tf \
+                       /tf_static \
+              --remap /sensing/camera/c1/image_rect_resized:=image \
+                      /localization/pose_twist_fusion_filter/biased_pose_with_covariance:=initial_pose_with_covariance
+
+mkdir -p ./result_images/trial/log
+mv ~/.ros/log/* ./result_images/trial/log/
+
+python3 python/analyze_particles_log.py ./result_images/trial/particles/
+python3 python/concat_test_images_result.py ./result_images/trial/
+python3 python/analyze_ros2_log.py ./result_images/trial/log/nerf_based_localizer*
