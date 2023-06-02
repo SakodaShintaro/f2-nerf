c76d8f4f71c2e15e8438d45df02fc95fdef29c1c
Shintaro Sakoda <sakoda@keio.jp>
2023-06-02 14:42:24 +0900

    Cleaned up topic names

--- ros2/script/launch_rosbag.sh
+++ ros2/script/launch_rosbag.sh
@@ -14,13 +14,17 @@ rm -rf ./result_images/trial
 
 ros2 service call /trigger_node_srv std_srvs/srv/SetBool "{data: true}"
 
+# IMAGE_TOPIC_NAME=/sensing/camera/c1/image_rect_resized
+IMAGE_TOPIC_NAME=/sensing/camera/traffic_light/image_raw
+POSE_TOPIC_NAME=/localization/pose_twist_fusion_filter/biased_pose_with_covariance
+
 ros2 bag play --rate 1 $1 \
-              --topics /sensing/camera/c1/image_rect_resized \
-                       /localization/pose_twist_fusion_filter/biased_pose_with_covariance \
+              --topics ${IMAGE_TOPIC_NAME} \
+                       ${POSE_TOPIC_NAME} \
                        /tf \
                        /tf_static \
-              --remap /sensing/camera/c1/image_rect_resized:=image \
-                      /localization/pose_twist_fusion_filter/biased_pose_with_covariance:=initial_pose_with_covariance
+              --remap ${IMAGE_TOPIC_NAME}:=image \
+                      ${POSE_TOPIC_NAME}:=initial_pose_with_covariance
 
 mkdir -p ./result_images/trial/log
 mv ~/.ros/log/* ./result_images/trial/log/
