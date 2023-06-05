45caf5fbc9a2bff9a119a9c928b26924e74749bf
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2023-06-05 11:02:11 +0900

    Changed params-file path

--- ros2/script/launch_nerf_based_localizer.sh
+++ ros2/script/launch_nerf_based_localizer.sh
@@ -12,7 +12,7 @@ set -u
 
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/root/f2-nerf/External/libtorch/lib/
 ros2 run ros2-f2-nerf nerf_based_localizer \
-    --ros-args --params-file ./src/ros2-f2-nerf/config/parameters.yaml \
+    --ros-args --params-file ./src/ros2-f2-nerf/config/parameters1.yaml \
     --ros-args --remap image:=/sensing/camera/c1/image_rect_resized \
     --ros-args --remap initial_pose_with_covariance:=/localization/pose_twist_fusion_filter/biased_pose_with_covariance \
     --ros-args --remap nerf_service:=/localization/pose_estimator/ndt_align_srv \
