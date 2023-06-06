75ba96e6d0570a63afb39f82ef8831d30f746b80
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2023-06-06 14:58:22 +0900

    Added awsim setting

--- ros2/script/launch_nerf_based_localizer.sh
+++ ros2/script/launch_nerf_based_localizer.sh
@@ -18,3 +18,12 @@ ros2 run ros2-f2-nerf nerf_based_localizer \
     --ros-args --remap nerf_service:=/localization/pose_estimator/ndt_align_srv \
     --ros-args --remap trigger_node_srv:=/localization/pose_estimator/trigger_node \
     --ros-args --remap nerf_pose_with_covariance:=/localization/pose_estimator/pose_with_covariance
+
+# export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/root/f2-nerf/External/libtorch/lib/
+# ros2 run ros2-f2-nerf nerf_based_localizer \
+#     --ros-args --params-file ./src/ros2-f2-nerf/config/parameters_awsim.yaml \
+#     --ros-args --remap image:=/sensing/camera/traffic_light/image_raw \
+#     --ros-args --remap initial_pose_with_covariance:=/localization/pose_twist_fusion_filter/biased_pose_with_covariance \
+#     --ros-args --remap nerf_service:=/localization/pose_estimator/ndt_align_srv \
+#     --ros-args --remap trigger_node_srv:=/localization/pose_estimator/trigger_node \
+#     --ros-args --remap nerf_pose_with_covariance:=/localization/pose_estimator/pose_with_covariance
