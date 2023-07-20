c6bcc9ee33d1d32307cd6f490eb106fd4d50e2b2
Shintaro Sakoda <sakoda@keio.jp>
2023-07-20 22:00:48 +0900

    Removed distortion kernel
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- src/Dataset/Dataset.cu
+++ src/Dataset/Dataset.cu
@@ -10,64 +10,6 @@
 
 using Tensor = torch::Tensor;
 
-template <typename T>
-__device__ __host__ inline void apply_camera_distortion(const T* extra_params, const T u, const T v, T* du, T* dv) {
-  const T k1 = extra_params[0];
-  const T k2 = extra_params[1];
-  const T p1 = extra_params[2];
-  const T p2 = extra_params[3];
-
-  const T u2 = u * u;
-  const T uv = u * v;
-  const T v2 = v * v;
-  const T r2 = u2 + v2;
-  const T radial = k1 * r2 + k2 * r2 * r2;
-  *du = u * radial + T(2) * p1 * uv + p2 * (r2 + T(2) * u2);
-  *dv = v * radial + T(2) * p2 * uv + p1 * (r2 + T(2) * v2);
-}
-
-// This implementation is from instant-ngp
-template <typename T>
-__device__ __host__ inline void iterative_camera_undistortion(const T* params, T* u, T* v) {
-  // Parameters for Newton iteration using numerical differentiation with
-  // central differences, 100 iterations should be enough even for complex
-  // camera models with higher order terms.
-  const uint32_t kNumIterations = 100;
-  const float kMaxStepNorm = 1e-10f;
-  const float kRelStepSize = 1e-6f;
-
-  Eigen::Matrix2f J;
-  const Eigen::Vector2f x0(*u, *v);
-  Eigen::Vector2f x(*u, *v);
-  Eigen::Vector2f dx;
-  Eigen::Vector2f dx_0b;
-  Eigen::Vector2f dx_0f;
-  Eigen::Vector2f dx_1b;
-  Eigen::Vector2f dx_1f;
-
-  for (uint32_t i = 0; i < kNumIterations; ++i) {
-    const float step0 = std::max(std::numeric_limits<float>::epsilon(), std::abs(kRelStepSize * x(0)));
-    const float step1 = std::max(std::numeric_limits<float>::epsilon(), std::abs(kRelStepSize * x(1)));
-    apply_camera_distortion(params, x(0), x(1), &dx(0), &dx(1));
-    apply_camera_distortion(params, x(0) - step0, x(1), &dx_0b(0), &dx_0b(1));
-    apply_camera_distortion(params, x(0) + step0, x(1), &dx_0f(0), &dx_0f(1));
-    apply_camera_distortion(params, x(0), x(1) - step1, &dx_1b(0), &dx_1b(1));
-    apply_camera_distortion(params, x(0), x(1) + step1, &dx_1f(0), &dx_1f(1));
-    J(0, 0) = 1 + (dx_0f(0) - dx_0b(0)) / (2 * step0);
-    J(0, 1) = (dx_1f(0) - dx_1b(0)) / (2 * step1);
-    J(1, 0) = (dx_0f(1) - dx_0b(1)) / (2 * step0);
-    J(1, 1) = 1 + (dx_1f(1) - dx_1b(1)) / (2 * step1);
-    const Eigen::Vector2f step_x = J.inverse() * (x + dx - x0);
-    x -= step_x;
-    if (step_x.squaredNorm() < kMaxStepNorm) {
-      break;
-    }
-  }
-
-  *u = x(0);
-  *v = x(1);
-}
-
 __global__ void Img2WorldRayKernel(int n_rays,
                                    Watrix34f* poses,
                                    Watrix33f* intri,
@@ -89,7 +31,6 @@ __global__ void Img2WorldRayKernel(int n_rays,
 
   float u = (j - cx) / fx;
   float v = (i - cy) / fy;  // OpenCV style
-  // iterative_camera_undistortion<float>((float*) (dist_params + cam_idx), &u, &v);
   Wec3f dir = {u, -v, -1.f };  // OpenGL style
   out_rays_d[ray_idx] = poses[cam_idx].block<3, 3>(0, 0) * dir;
   out_rays_o[ray_idx] = poses[cam_idx].block<3, 1>(0, 3);
