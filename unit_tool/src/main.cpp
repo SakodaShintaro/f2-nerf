00e07ec76139eb2dc30d5efc010beb84688fc486
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2023-07-21 18:07:02 +0900

    Added save image
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- unit_tool/src/main.cpp
+++ unit_tool/src/main.cpp
@@ -1,4 +1,5 @@
 #include "../../ros2/src/ros2-f2-nerf/src/localizer_core.hpp"
+#include "../../src/Utils/Utils.h"
 
 int main()
 {
@@ -14,9 +15,22 @@ int main()
   for (int32_t i = 0; i < core.dataset_->n_images_; i++) {
     torch::Tensor initial_pose = core.dataset_->poses_[i];
     torch::Tensor image_tensor = core.dataset_->image_tensors_[i];
-    torch::Tensor optimized_pose = core.optimize_pose(initial_pose, image_tensor, iteration_num);
+    Utils::WriteImageTensor("image_01_gt.png", image_tensor);
+
+    auto [score_before, nerf_image_before] =
+      core.pred_image_and_calc_score(initial_pose, image_tensor);
+
     std::cout << "Number " << i << std::endl;
-    std::cout << "initial_pose\n" << initial_pose << std::endl;
+    std::cout << "score_before\n" << score_before << std::endl;
+    Utils::WriteImageTensor("image_02_before.png", nerf_image_before);
+
+    torch::Tensor optimized_pose = core.optimize_pose(initial_pose, image_tensor, iteration_num);
     std::cout << "optimized_pose\n" << optimized_pose << std::endl;
+    auto [score_after, nerf_image_after] =
+      core.pred_image_and_calc_score(optimized_pose, image_tensor);
+    std::cout << "score_after\n" << score_after << std::endl;
+    Utils::WriteImageTensor("image_03_after.png", nerf_image_after);
+
+    break;
   }
 }
