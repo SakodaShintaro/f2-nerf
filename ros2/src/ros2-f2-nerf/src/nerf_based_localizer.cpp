04cba8e1ad17a1b2199fdd700213c3b69fa331c4
Shintaro Sakoda <sakoda@keio.jp>
2023-07-23 07:55:44 +0900

    Added resize_factor to nerf_based_localizer.cpp
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- ros2/src/ros2-f2-nerf/src/nerf_based_localizer.cpp
+++ ros2/src/ros2-f2-nerf/src/nerf_based_localizer.cpp
@@ -40,6 +40,7 @@ NerfBasedLocalizer::NerfBasedLocalizer(
   param.noise_rotation_y = this->declare_parameter<float>("noise_rotation_y");
   param.noise_rotation_z = this->declare_parameter<float>("noise_rotation_z");
   param.is_awsim = is_awsim_;
+  param.resize_factor = 5;
   localizer_core_ = LocalizerCore(runtime_config_path, param);
 
   initial_pose_with_covariance_subscriber_ =
