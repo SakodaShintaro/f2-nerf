6c3254aa3a07e80f16b7eeed7d328b55c700d209
Shintaro Sakoda <sakoda@keio.jp>
2023-07-23 08:26:12 +0900

    Added runtime_config_path to command line
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- ros2/script/check.sh
+++ ros2/script/check.sh
@@ -2,8 +2,10 @@
 
 set -eux
 
-cd $(dirname $0)/../
 PARAMETER_FILE=$(readlink -f $1)
+RUNTIME_CONFIG=$(readlink -f $2)
+
+cd $(dirname $0)/../
 
 colcon build --symlink-install --packages-up-to ros2-f2-nerf --cmake-args -DCMAKE_BUILD_TYPE=Release
 
@@ -16,6 +18,7 @@ rm -rf ~/.ros/log/*
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/root/f2-nerf/External/libtorch/lib/
 ros2 run ros2-f2-nerf nerf_based_localizer \
     --ros-args --params-file $PARAMETER_FILE \
+    --ros-args --param runtime_config_path:=$RUNTIME_CONFIG \
     --ros-args --param save_image:=true \
     --ros-args --param save_particles:=true
 
--- ros2/script/regression_test.sh
+++ ros2/script/regression_test.sh
@@ -7,6 +7,11 @@ cd $(dirname $0)/../
 # スクリプトが終了する際にすべてのバックグラウンドプロセスを停止するためのトラップを設定
 trap "kill 0" EXIT
 
-./script/check.sh ./src/ros2-f2-nerf/config/parameters_awsim.yaml &
+./script/check.sh \
+    ./src/ros2-f2-nerf/config/parameters_awsim.yaml\
+    /home/sakoda/work/f2-nerf/exp/20230717_loop/test/record/runtime_config.yaml &
 
 ./script/launch_pose_and_image_publisher.sh ~/data/converted/AWSIM/20230717_loop/
+
+# 秒数表示
+echo "${SECONDS} sec"
