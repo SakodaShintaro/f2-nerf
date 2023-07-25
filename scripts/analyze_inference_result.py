dec64a696453cb5997365960a6ca528b6213db81
Shintaro Sakoda <sakoda@keio.jp>
2023-07-25 18:16:45 +0900

    Fixed plot script
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- scripts/analyze_inference_result.py
+++ scripts/analyze_inference_result.py
@@ -2,12 +2,11 @@
 """
 
 import argparse
-import cv2
 from glob import glob
-import subprocess
 import pandas as pd
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+import os
 
 
 def parse_args():
@@ -33,17 +32,15 @@ if __name__ == "__main__":
 
         for i in range(8):
             noised = df[df['name'] == f'noised_{i}']
-            optimized = df[df['name'] == f'optimized_{i}']
-
-            plt.scatter(noised['x'], noised['y'], c=noised['score'],
+            plt.scatter(noised['x'], noised['y'], s=50,
+                        color="red", label=f'Noised_{i}')
+            plt.scatter(noised['x'], noised['y'], c=noised['score'], s=25,
                         cmap='viridis', vmin=min_score, vmax=max_score, label=f'Noised_{i}')
-            plt.scatter(optimized['x'], optimized['y'],
-                        c=optimized['score'], cmap='viridis', vmin=min_score, vmax=max_score, label=f'Optimized_{i}')
+            for j in range(20):
+                optimized = df[df['name'] == f'optimized_{i}_{j:02d}']
 
-            # Draw an arrow from the noised position to the optimized position
-            plt.annotate('', xy=(optimized['x'].values[0], optimized['y'].values[0]),
-                         xytext=(noised['x'].values[0], noised['y'].values[0]),
-                         arrowprops=dict(facecolor='gray', shrink=0.05))
+                plt.scatter(optimized['x'], optimized['y'],
+                            c=optimized['score'], cmap='viridis', vmin=min_score, vmax=max_score, label=f'Optimized_{i}_{j:02d}')
 
         plt.xlabel('x')
         plt.ylabel('y')
@@ -51,3 +48,4 @@ if __name__ == "__main__":
         plt.savefig(f"{frame_dir}/plot_result.png",
                     bbox_inches="tight", pad_inches=0.05)
         plt.close()
+        print(os.path.abspath(f"{frame_dir}/plot_result.png"))
