3ff6e8847e13f2dd73cf9bb2893e82f1d60dcdf6
Shintaro Sakoda <sakoda@keio.jp>
2023-08-09 08:05:07 +0900

    Added plot last
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- scripts/analyze_inference_result.py
+++ scripts/analyze_inference_result.py
@@ -19,6 +19,8 @@ if __name__ == "__main__":
     args = parse_args()
     result_dir = args.result_dir
 
+    ITERATION = 10
+
     frame_dir_list = sorted(glob(f"{result_dir}/*/"))
     for frame_dir in tqdm(frame_dir_list):
         df = pd.read_csv(f"{frame_dir}/position.tsv", sep="\t")
@@ -36,7 +38,7 @@ if __name__ == "__main__":
                         color="red", label=f'Noised_{i}')
             plt.scatter(noised['x'], noised['y'], c=noised['score'], s=25,
                         cmap='viridis', vmin=min_score, vmax=max_score, label=f'Noised_{i}')
-            for j in range(20):
+            for j in range(ITERATION):
                 optimized = df[df['name'] == f'optimized_{i}_{j:02d}']
 
                 plt.scatter(optimized['x'], optimized['y'],
@@ -46,6 +48,37 @@ if __name__ == "__main__":
         plt.ylabel('y')
         plt.colorbar(label='Score')
         save_path = os.path.abspath(f"{frame_dir}/plot_result.png")
+        plt.savefig(save_path, bbox_inches="tight", pad_inches=0.05)
+        plt.close()
+        print(f"Saved to {save_path}")
+
+        # print only last pos
+        original = df[df['name'] == 'original']
+        plt.scatter(original['x'], original['y'], c=original['score'],
+                    cmap='viridis', vmin=min_score, vmax=max_score, label='Original')
+
+        for i in range(8):
+            noised = df[df['name'] == f'noised_{i}']
+            nx = noised['x'].values[0]
+            ny = noised['y'].values[0]
+            plt.scatter(nx, ny, s=50, color="red", label=f'Noised_{i}')
+            plt.scatter(nx, ny, c=noised['score'], s=25,
+                        cmap='viridis', vmin=min_score, vmax=max_score, label=f'Noised_{i}')
+            optimized = df[df['name'] == f'optimized_{i}_{ITERATION - 1:02d}']
+            ox = optimized['x'].values[0]
+            oy = optimized['y'].values[0]
+            plt.scatter(ox, oy,
+                        c=optimized['score'], cmap='viridis', vmin=min_score, vmax=max_score, label=f'Optimized_{i}_{j:02d}')
+
+            # plot arrow noised -> optimized
+            plt.arrow(nx, ny, ox - nx, oy - ny, length_includes_head=True,
+                      head_width=0.05, head_length=0.05, fc='k', ec='k')
+
+
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.colorbar(label='Score')
+        save_path = os.path.abspath(f"{frame_dir}/plot_result_last.png")
         plt.savefig(save_path, bbox_inches="tight", pad_inches=0.05)
         plt.close()
         print(f"Saved to {save_path}")
