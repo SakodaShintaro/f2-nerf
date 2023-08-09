4164ee6fb425088d2e9d0548451a49669ad66906
Shintaro Sakoda <sakoda@keio.jp>
2023-08-09 11:16:38 +0900

    Removed grid_encoder
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- src/Field/field.cmake
+++ src/Field/field.cmake
@@ -2,6 +2,4 @@ set(CUR_DIR ${PROJECT_SOURCE_DIR}/src/Field)
 list(APPEND SRC_FIELD
         ${CUR_DIR}/Hash3DAnchored.cpp
         ${CUR_DIR}/Hash3DAnchored.cu
-        ${CUR_DIR}/GridEncoder.cpp
-        ${CUR_DIR}/gridencoder.cu
         ${CUR_DIR}/TCNNWP.cpp)
