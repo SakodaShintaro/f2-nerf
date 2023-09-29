688d51ac4fb6e86a19675ac5eaf0e9bc0b6f509b
Shintaro Sakoda <sakoda@keio.jp>
2023-09-29 17:22:38 +0900

    Fixed filenames
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- src/Utils/Utils.h
+++ src/Utils/Utils.h
@@ -8,10 +8,6 @@
 namespace Utils {
 using Tensor = torch::Tensor;
 
-void TensorExportPCD(const std::string& path, Tensor verts);
-void TensorExportPCD(const std::string& path, Tensor verts, Tensor vert_colors);
-int SaveVectorAsNpy(const std::string& path, std::vector<float> data);
-
 Tensor ReadImageTensor(const std::string& path);
 bool WriteImageTensor(const std::string& path, Tensor img);
 
